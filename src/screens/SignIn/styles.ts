f4c2684b9cd76a2b384e52b409778bb052567e98
marrcelosantana <marcelosantana@alu.ufc.br>
2023-04-24 16:46:38 -0300

    [GoFinances-App] - Init Pages

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.BLUE};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`;
