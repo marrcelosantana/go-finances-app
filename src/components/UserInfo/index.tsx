d472c5440158232adf5776fe3d2b95610528485b
marrcelosantana <marcelosantana@alu.ufc.br>
2023-04-29 14:03:02 -0300

    [GoFinances-App] - Update assets

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,11 +1,12 @@
-import avatar from "../../assets/eu.jpg";
-
 import { Avatar, Container, Info, Title, Username } from "./styles";
 
 export function UserInfo() {
   return (
     <Container>
-      <Avatar source={avatar} resizeMode="cover" />
+      <Avatar
+        source={{ uri: "http://github.com/marrcelosantana.png" }}
+        resizeMode="cover"
+      />
 
       <Info>
         <Title>Olá,</Title>
